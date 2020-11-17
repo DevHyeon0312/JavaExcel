f7ef171b8b30048d218d86d27deea0b47e4c5d88
devhyeon <devhyeon0312@gmail.com>
2020-11-17 10:41:47 +0900

    FolderUtils - Create Folder - Delete Folder - Delete Files in Folder

---
+++ src/Folder/FolderUtils.java
@@ -0,0 +1,73 @@
+package Folder;
+
+import java.io.File;
+
+/**
+ * Created by DevHyeon on 2020.11.17
+ * PC : MacBook Pro (16-inch, 2019)
+ * OS Version : macOS Catalina - 10.15.7(19H2)
+ * */
+
+public class FolderUtils {
+
+    /* Folder Create */
+    public static void createFolder() {
+        String root = new File("").getPath();
+        String path = root+"Excels"; //폴더 경로
+        File Folder = new File(path);
+
+        //if not exist Created Folder.
+        if (!Folder.exists()) {
+            try{
+                Folder.mkdir(); //Created Folder
+                System.out.println(path+"SUCCESS.");
+            }
+            catch(Exception e){
+                e.getStackTrace();
+                System.out.println(path+"FAIL.");
+            }
+        } else {
+            System.out.println(path+"EXISTED.");
+        }
+    }
+
+    /* Folder Delete */
+    public static void deleteFolder() {
+        String root = new File("").getPath();
+        String path = root+"Excels"; //폴더 경로
+        File Folder = new File(path);
+
+        while(Folder.exists()) {
+            File[] folder_list = Folder.listFiles(); //파일리스트 얻어오기
+
+            for (int j = 0; j < folder_list.length; j++) {
+                folder_list[j].delete(); //파일 삭제
+                System.out.println("파일이 삭제되었습니다.");
+
+            }
+
+            if(folder_list.length == 0 && Folder.isDirectory()){
+                Folder.delete(); //대상폴더 삭제
+                System.out.println("폴더가 삭제되었습니다.");
+            }
+        }
+    }
+
+
+    /* Files Delete in Folder */
+    public static void deleteFiles() {
+        String root = new File("").getPath();
+        String path = root+"Excels"; //폴더 경로
+        File Folder = new File(path);
+
+        while(Folder.exists()) {
+            File[] folder_list = Folder.listFiles(); //파일리스트 얻어오기
+
+            for (int j = 0; j < folder_list.length; j++) {
+                folder_list[j].delete(); //파일 삭제
+                System.out.println("파일이 삭제되었습니다.");
+
+            }
+        }
+    }
+}
--- src/MainUI.java
+++ src/MainUI.java
@@ -1,8 +1,35 @@
+import Data.User;
+import Folder.FolderUtils;
+
+import java.util.ArrayList;
+
+/**
+ * Created by DevHyeon on 2020.11.17
+ * */
 public class MainUI {
 
     public static void main(String[] args) {
         System.out.println("Create Project");
+        //Create Folder
+//        FolderUtils.createFolder();
+
+//        FolderUtils.deleteFolder();
+
+        FolderUtils.deleteFiles();
     }
+
+//    private static ArrayList<User> createList() {
+//        ArrayList<User> users = new ArrayList<>();
+//        for (int i=0; i<10; i++) {
+//            User user = new User();
+//            user.setUserUnqId("KEY"+i);
+//            user.setUserName("DevHyeon"+i);
+//            user.setUserAge(20+i);
+//            user.setUserGender("Gender");
+//            user.setUserBirth("birth:"+i);
+//        }
+//        return users;
+//    }
 }
 
 
